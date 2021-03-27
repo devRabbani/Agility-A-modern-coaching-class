d54c41e25879c5a5be0cbeedbe8dd7e8965e45bc
StrikerOne65 <golamrabbani20014@gmail.com>
2021-03-27 16:03:55 +0530

    beautify

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-window.addEventListener("load",()=>{document.querySelector("html").style.overflowY="auto",document.querySelector(".loader").style.display="none",document.querySelectorAll(".animatedWait").forEach(e=>{e.classList.remove("animatedWait")})}),document.querySelector(".nav span").addEventListener("click",()=>{document.querySelector("#myMenu").classList.toggle("slide")}),document.querySelectorAll(".nav li a").forEach(e=>{e.addEventListener("click",()=>{document.querySelector("#myMenu").classList.toggle("slide")})}),document.querySelectorAll("a").forEach(e=>{var t=e.getAttribute("href");t&&t.includes("#")&&e.addEventListener("click",e=>{e.preventDefault(),document.getElementById(t.replace(/#/g,"")).scrollIntoView({behavior:"smooth",block:"start",inline:"nearest"})})});
+window.addEventListener("load", () => {
+    document.querySelector("html").style.overflowY = "auto", document.querySelector(".loader").style.display = "none", document.querySelectorAll(".animatedWait").forEach(e => {
+        e.classList.remove("animatedWait")
+    })
+}), document.querySelector(".nav span").addEventListener("click", () => {
+    document.querySelector("#myMenu").classList.toggle("slide")
+}), document.querySelectorAll(".nav li a").forEach(e => {
+    e.addEventListener("click", () => {
+        document.querySelector("#myMenu").classList.toggle("slide")
+    })
+}), document.querySelectorAll("a").forEach(e => {
+    var t = e.getAttribute("href");
+    t && t.includes("#") && e.addEventListener("click", e => {
+        e.preventDefault(), document.getElementById(t.replace(/#/g, "")).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        })
+    })
+});
